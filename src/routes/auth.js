ac3a0374f564fbbf33e25ca7676ac2589c92ca1c
Ankit Borbora <ankit.borbora01@gmail.com>
2025-08-16 19:55:17 +0530

    using express router

---
+++ src/routes/auth.js
@@ -0,0 +1,107 @@
+const express= require("express");
+const bcrypt= require("bcrypt");
+const { validateSignUpData } = require("../utils/validation.js");
+const User = require("../models/user.js");
+const validator = require("validator");
+
+const authRouter= express.Router();
+
+authRouter.post("/signup",async (req,res)=>{
+
+    try{
+    
+    validateSignUpData(req.body);
+
+    const { firstName, lastName, emailId, password, age, gender, image, about, skills} = req?.body;
+    let hashedPass= await bcrypt.hash(password,10);
+
+    let defaultImg="https://geographyandyou.com/images/user-profile.png";
+
+    let profileImg= (!image || image=="")?defaultImg:image;
+
+    let insertionObj={
+        firstName,
+        lastName,
+        emailId,
+        password:hashedPass,
+        age,
+        gender,
+        image:profileImg,
+        about,
+        skills
+    }
+
+
+
+    let insertDoc= await User.create(insertionObj);
+
+    if(insertDoc){
+        return res.status(200).json({
+            code:200,
+            message:"Data inserted successfully",
+            data: insertDoc
+        });
+    }
+    return res.status(400).json({
+        code:400,
+        message:"Data not inserted"
+    });
+
+    }
+    catch(err){
+        return res.status(500).json({
+            code:500,
+            message:"Some error ocurred: "+err.message
+        }); 
+    }
+
+});
+
+authRouter.post("/login",async (req,res)=>{
+    
+    try{
+        const { emailId, password} = req?.body;
+
+        if(!validator.isEmail(emailId)){
+            return res.status(400).json({
+                code:400,
+                message:"Please provide valid email"
+            });
+        }
+
+        let user = await User.findOne({emailId});
+
+        if(!user){
+            return res.status(400).json({
+                code:400,
+                message:"Invalid email"
+            });
+        }
+
+        const isValidPassword = await user.validatePassword(password);
+        
+        if(isValidPassword){
+            const token = await user.getJWT();
+            res.cookie("token",token);
+            return res.status(200).json({
+                code:200,
+                message:"Logged in successfully"
+            });
+        }
+
+        return res.status(400).json({
+            code:400,
+            message:"Invalid password"
+        });
+
+    }
+    catch(err){
+        return res.status(500).json({
+            code:500,
+            message:"Some error ocurred: "+err.message
+        }); 
+    }
+});
+
+
+module.exports=authRouter;
---
+++ src/routes/profile.js
@@ -0,0 +1,198 @@
+const express = require("express");
+const { userAuth } = require("../middlewares/auth.js");
+const User = require("../models/user.js");
+
+
+
+const profileRouter = express.Router();
+
+profileRouter.get("/profile",userAuth, async (req,res)=>{
+    
+    try{
+
+        let data = req.user;
+
+        return res.status(200).json({
+            code:200,
+            message:"Data fetched successfully",
+            data
+        });
+
+    }
+    catch(err){
+        return res.status(500).json({
+            code:500,
+            message:"Some error ocurred: "+err.message
+        }); 
+    }
+});
+
+profileRouter.get("/feed",async (req,res)=>{
+
+    try{
+        let users = await User.find({});
+
+        if(users.length>0){
+            return res.status(200).json({
+                code:200,
+                message:"Data found successfully",
+                data: users
+            });
+        }
+
+        return res.status(404).json({
+            code:404,
+            message:"Data not found"
+        });
+    }
+    catch(err){
+        return res.status(500).json({
+            code:500,
+            message:"Some error ocurred: "+err.message
+        }); 
+    }
+    
+
+
+});
+
+profileRouter.get("/user", async(req,res)=>{
+
+    let { email } = req?.body;
+
+    try{
+
+        let user = await User.findOne({emailId:email});
+
+        if(user){
+            return res.status(200).json({
+                code:200,
+                message:"Data found successfully",
+                data: user
+            });
+        }
+
+        return res.status(404).json({
+            code:404,
+            message:"Data not found"
+        });
+    }
+    catch(err){
+        return res.status(500).json({
+            code:500,
+            message:"Some error ocurred: "+err.message
+        }); 
+    }
+
+});
+
+profileRouter.delete("/delete",async (req,res)=>{
+    try{
+
+        let {userId}= req?.body;
+
+        let deletedDoc = await User.findByIdAndDelete(userId);
+
+        if(deletedDoc){
+            return res.status(200).json({
+                code:200,
+                message:"Data deleted successfully"
+            });
+        }
+        return res.status(400).json({
+            code:400,
+            message:"Unable to delete data"
+        });
+
+    }
+    catch(err){
+        return res.status(500).json({
+            code:500,
+            message:"Some error ocurred: "+err.message
+        }); 
+    }
+
+});
+
+profileRouter.patch("/update/:userId",async (req,res)=>{
+
+    let userId= req.params?.userId;
+
+    if(!userId){
+        return res.status(400).json({
+            code:400,
+            message:"Please provide a valid userId"
+        });
+    }
+
+    try{
+
+        let data = req?.body;
+
+        Object.keys(data).forEach(function(val){
+            if(val=="emailId")throw new Error("Update ot allowed for email");
+        });
+        let update = await User.findByIdAndUpdate(userId,req.body,{runValidators:true});
+
+        if(update){
+            return res.status(200).json({
+                code:200,
+                message:"Data updated successfully"
+            });
+        }
+        return res.status(400).json({
+            code:400,
+            message:"Unable to update data"
+        });
+
+    }
+    catch(err){
+        return res.status(500).json({
+            code:500,
+            message:"Some error ocurred: "+err.message
+        }); 
+    }
+});
+
+profileRouter.patch("/update-by-email/:email",async (req,res)=>{
+
+    let email = req.params?.email;
+
+    if(!email){
+        return res.status(400).json({
+            code:400,
+            message:"Please provide a valid email"
+        });
+    }
+
+    try{
+
+        let data = req?.body;
+
+        Object.keys(data).forEach(function(val){
+            if(val=="emailId")throw new Error("Update ot allowed for email");
+        });
+
+        let update = await User.updateOne({emailId:email},req.body,{runValidators:true});
+
+        if(update.modifiedCount>0){
+            return res.status(200).json({
+                code:200,
+                message:"Data updated successfully"
+            });
+        }
+        return res.status(400).json({
+            code:400,
+            message:"Unable to update data"
+        });
+
+    }
+    catch(err){
+        return res.status(500).json({
+            code:500,
+            message:"Some error ocurred: "+err.message
+        }); 
+    }
+});
+
+module.exports=profileRouter;
